ea753647811e2485541cd10893b7775c970f19df
jith <jith87@gmail.com>
2016-06-11 02:46:23 -0400

    Fixing broken support for Object Mapper library

--- Delphos/SchoolBusiness.swift
+++ Delphos/SchoolBusiness.swift
@@ -10,10 +10,10 @@ import Foundation
 
 
 let DEBUG: Bool = true
-//let DEV_URL: String = "http://ceti-test-env.elasticbeanstalk.com"
-//let DEV_TARGET: String  =  "http://ceti-test-env.elasticbeanstalk.com/api/"
-let DEV_URL: String = "http://192.168.1.3:3000"
-let DEV_TARGET: String  =  "http://192.168.1.3:3000/api/"
+let DEV_URL: String = "http://ceti-test-env.elasticbeanstalk.com"
+let DEV_TARGET: String  =  "http://ceti-test-env.elasticbeanstalk.com/api/"
+//let DEV_URL: String = "http://192.168.1.3:3000"
+//let DEV_TARGET: String  =  "http://192.168.1.3:3000/api/"
 let PRO_URL: String = "https://www.school2biz.com";
 let PRO_TARGETL: String =  "https://www.school2biz.com/api/"
 let AWS_S3: String = "https://s3-us-west-1.amazonaws.com/ceti-sb/badges/"
