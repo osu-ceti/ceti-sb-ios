79cc968c87bf667032c157e40e903859f202da28
jith <jith87@gmail.com>
2016-12-02 15:11:19 +0530

    Added Pods folder - for build issues

---
+++ Pods/Target Support Files/Pods-Delphos/Pods-Delphos-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Delphos : NSObject
+@end
+@implementation PodsDummy_Pods_Delphos
+@end
---
+++ Pods/Target Support Files/Pods-DelphosTests/Pods-DelphosTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DelphosTests : NSObject
+@end
+@implementation PodsDummy_Pods_DelphosTests
+@end
